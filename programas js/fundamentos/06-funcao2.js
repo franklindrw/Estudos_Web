c0b9199edc26ce8ce143c5f9aa4eb6be98cea8f7
Franklin Almeida Campos <81038899+franklindrw@users.noreply.github.com>
2021-10-23 17:31:35 -0300

    aulas js 23-10

---
+++ programas js/fundamentos/06-funcao2.js
@@ -0,0 +1,27 @@
+// Armazenando uma função em uma variavel
+
+const imprimirSoma = function(a, b){
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+console.log("============================================")
+
+// Armazenando uma função arrow em uma variavel
+const soma = (a, b) =>{
+    return a + b
+}
+
+console.log(soma(2, 7))
+
+console.log("============================================")
+
+// Retorno implícito
+const subtracao = (a, b) => a - b
+console.log(subtracao(2, 3))
+
+const imprimir2 = a => console.log(a)
+imprimir2('Legal!')
+
+
