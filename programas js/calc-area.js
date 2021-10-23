503bdcf93b4ad6ed661d07832fdb4de8f8071a40
Franklin Almeida Campos <81038899+franklindrw@users.noreply.github.com>
2021-10-23 00:04:23 -0300

    estudos js

---
+++ programas js/calc-area.js
@@ -0,0 +1,9 @@
+//PI * Raio * raio
+
+let raio = 10;
+
+const pi = 3.141;
+
+let resultado = pi * raio * raio;
+
+console.log("Área em Metros: "+resultado + " m2.");
---
+++ programas js/troca_valor.js
@@ -0,0 +1,10 @@
+let a = 7;
+let b = 94;
+
+let temp = a;
+
+a = b;
+b = temp;
+
+console.log("Valor A: "+ a);
+console.log("Valor B: "+ b);
